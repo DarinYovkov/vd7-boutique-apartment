d089774759776a46d9bfca1856372c5e15a334f7
DarinYovkov <yovkov.remaxprestige@gmail.com>
2026-06-16 00:25:02 +0300

    Update script.js

--- css/js/script.js
+++ css/js/script.js
@@ -1,1 +1,9 @@
 
+// =====================================
+// VD7 Boutique Apartment
+// Main JavaScript File
+// =====================================
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("VD7 Boutique Apartment loaded successfully.");
+});
